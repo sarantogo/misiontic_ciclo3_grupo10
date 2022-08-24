1a1329f30af6e549c226fcecc2ecca74576af712
sarantogo <111017996+sarantogo@users.noreply.github.com>
2022-08-24 17:59:49 -0500

    create method saludar() in HolaMundo.java

--- intro-git/HolaMundo.java
+++ intro-git/HolaMundo.java
@@ -1,5 +1,11 @@
 class HolaMundo {
     public static void main(String[] args) {
+        saludar("Sara");
+
+    }
+
+    public static void saludar(String nombre) {
+        System.out.println("¡Hola " + nombre + "!");
 
     }
 }
