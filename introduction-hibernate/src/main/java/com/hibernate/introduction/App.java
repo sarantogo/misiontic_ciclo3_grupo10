b7c08ff62bc55948db624ba49fa87dfd3dc879f0
sarantogo <111017996+sarantogo@users.noreply.github.com>
2022-09-12 16:29:21 -0500

    update hibernate.introduction

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -3,7 +3,6 @@ package com.hibernate.introduction;
 import com.hibernate.introduction.controlador.PersonaController;
 import com.hibernate.introduction.vista.PersonaView;
 
-
 public class App {
     public static void main(String[] args) {
         PersonaController controller = new PersonaController();
